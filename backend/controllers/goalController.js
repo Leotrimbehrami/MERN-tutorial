a1fabc7efecef21074190a1303cc090f13672b69
dci-student <user@user-thinkpad-l15.fritz.box>
2023-07-05 01:18:14 +0200

    added asyncHandler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,15 +1,18 @@
+const asyncHandler = require("express-async-handler")
 // @ desc  Get Goals
 // @route  Get /api/goals
 // @acces  Private
-const getGoals = (req,res) => {
+
+const getGoals = asyncHandler ( async (req,res) => {
+  //console.log(req.body)
   res.status(200).json({message: "Get goals"})
-}
+})
 
 
 // @ desc  set Goals
 // @route  POST /api/goals
 // @acces  Private
-const setGoal = (req,res) => {
+const setGoal =  asyncHandler (async (req,res) => {
   if(!req.body.text) {
     res.status(400)
     throw new Error("Please add a text field")
@@ -17,21 +20,21 @@ const setGoal = (req,res) => {
 
 
   res.status(200).json({message: "Set goal"})
-}
+})
 
 // @ desc  Update Goal
 // @route  PUT /api/goals/:id
 // @acces  Private
-const updateGoal = (req,res) => {
+const updateGoal = asyncHandler ( async (req,res) => {
   res.status(200).json({message: `Update goal ${req.params.id}`})
-}
+})
 
 // @ desc  Delete Goals
 // @route  DELETE /api/goals/:id
 // @acces  Private
-const deleteGoal = (req,res) => {
+const deleteGoal = asyncHandler( async (req,res) => {
   res.status(200).json({message: `Delete goal ${req.params.id}`})
-}
+})
 
 module.exports = {
   getGoals,
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const exp = require("constants")
 const express = require("express")
 const dotenv = require("dotenv").config()
+const { errorHandler } = require("./middleware/errorMiddleware.js")
 const port = process.env.PORT  || 5000
 
 const app = express()
@@ -11,6 +12,9 @@ app.use(express.urlencoded({ extended: false}))
 
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+
+// overwrite the default express error handler
+app.use(errorHandler)
 
 
 
