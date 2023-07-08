8765b0ff96e7b81abd1c2a7d740ef043198948bc
dci-student <user@user-thinkpad-l15.fritz.box>
2023-07-08 18:33:27 +0200

    generated JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,7 +38,8 @@ const registerUser =asyncHandler( async (req,res) => {
       res.status(201).json({
         _id: user.id,
         name: user.name,
-        email: user.email
+        email: user.email,
+        token: generateToken(user._id)
       })
     } else {
       res.status(400)
@@ -55,9 +56,23 @@ const registerUser =asyncHandler( async (req,res) => {
 // @access  Public
 
 const loginUser =asyncHandler( async(req,res) => {
-  res.json({message: 'Login User'
+  const { email, password } = req.body
+
+  // check for user email
+  const user = await User.findOne({email})
+
+  if(user && (await bcrypt.compare(password, user.password))){
+    res.json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id)
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid credentials')
+  }
   })
-})
 
 // @ desc   Get user data
 // @route  GET0 /api/users/me
@@ -67,6 +82,14 @@ const getMe =asyncHandler( async (req,res) => {
   res.json({message: 'User data display'
   })
 })
+
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '90d',
+  })
+}
 
 
 module.exports = {
