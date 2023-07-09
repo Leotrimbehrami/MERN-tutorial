5a883a4a37cf5748fc15f9ee4308868c09ba4eee
dci-student <user@user-thinkpad-l15.fritz.box>
2023-07-09 15:52:14 +0200

    started with frontend and created frontend folder, components, React Router Setup, Header and Navigation

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react"
+import { FaUser } from "react-icons/fa"
+
+function Register() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  })
+
+  const {name, email, password, password2} = formData
+
+  const onChange = () => {}
+
+
+
+
+  return (
+  <>
+    <section className="heading">
+      <h1>
+        <FaUser /> Register
+      </h1>
+      <p>Please create an account</p>
+    </section>
+
+    <section className="form">
+      <form>
+        <div className="form-group">
+          <input 
+            type="text"
+            className="form-controll"
+            id="name" name="name"
+            value={name} 
+            placeholder="Enter your Name"
+            onChange={onChange}
+          />
+        </div>
+      </form>
+    </section>
+  </>
+
+
+  )
+}
+
+export default Register
