1769308fbe8073b0f332928b562fa1f6aab6e238
dci-student <user@user-thinkpad-l15.fritz.box>
2023-07-12 00:35:47 +0200

    added goalForm and Item

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import { ToastContainer } from 'react-toastify';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import Header from './components/Header';
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Header from './components/Header'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
             <Route path='/' element={<Dashboard />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
-          </Routes> 
+          </Routes>
         </div>
       </Router>
       <ToastContainer />
@@ -24,4 +24,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,13 +1,8 @@
-
-
 function Spinner() {
   return (
-    <div className="loadingSpinnerContainer">
-      <div className="loadingSpinner">
-          Spinner
-      </div>
+    <div className='loadingSpinnerContainer'>
+      <div className='loadingSpinner'></div>
     </div>
-      
   )
 }
 
